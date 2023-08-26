d8584a617084b0b2038883657443f43b78346f11
MisterVaidas <129537139+MisterVaidas@users.noreply.github.com>
2023-08-26 23:26:07 +0100

    second dice added to my app

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -29,11 +29,24 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
+        val dice2 = Dice2(6)
+        val diceRoll2 = dice2.roll2()
+
         // Find the image view in the layout
         val diceImage: ImageView = findViewById(R.id.imageView2)
+        val diceImage2: ImageView = findViewById(R.id.imageView3)
 
         // Determine which drawable resource ID to use based on the dice roll
-        val drawableResource = when (diceRoll) {
+        val drawableResource1 = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val drawableResource2 = when (diceRoll2) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -43,10 +56,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Update the ImageView with the correct drawable resource ID
-        diceImage.setImageResource(drawableResource)
+        diceImage.setImageResource(drawableResource1)
+        diceImage2.setImageResource(drawableResource2)
 
         //Update the content description
         diceImage.contentDescription = diceRoll.toString()
+        diceImage2.contentDescription = diceRoll2.toString()
     }
 }
 
@@ -54,5 +69,12 @@ class Dice(private val numSides: Int) {
 
     fun roll(): Int {
         return (1..numSides).random()
+    }
+}
+
+class Dice2(private val numSides2: Int) {
+
+    fun roll2(): Int {
+        return (1..numSides2).random()
     }
 }
